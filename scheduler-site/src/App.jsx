df574e8a0c55260e30d2200a9dfb35e1cd56e2de
Florian Disterhoft <flodisterhoft-ops@users.noreply.github.com>
2026-04-27 13:53:48 -0700

    Restore notify button styling

--- scheduler-site/src/App.jsx
+++ scheduler-site/src/App.jsx
@@ -586,6 +586,7 @@ function EventCard({ event, user, isAdmin, isManager, doAction, onNotify, onAssi
               aria-hidden={!isManager}
             >
               <span aria-hidden="true">{'\uD83D\uDCE8'}</span>
+              <span className="notify-btn-label">Notify</span>
             </button>
           )}
         </div>
